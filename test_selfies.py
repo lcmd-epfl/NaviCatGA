1ee8bc490acebd91830641ed401402689ae63626
rlaplaza <rlaplaza@lct.jussieu.fr>
2021-01-09 04:04:32 +0100

    pytest runs a-ok now

--- test_selfies.py
+++ test_selfies.py
@@ -30,7 +30,7 @@ def test_selfies():
             range(n_starting_genes)
         ),  # We do not modify the melatonin molecule
         fitness_function=fitness_function_selfies(1),  # See fitness_functions_selfies
-        max_gen=200,  # This is a simple test
+        max_gen=100,  # This is a simple test
         logger_file="logp.log",
         verbose=True,
     )
@@ -56,7 +56,7 @@ def test_selfies():
         fitness_function=fitness_function_selfies(
             2
         ),  # See fitness_functions_selfies, this is inverse logp
-        max_gen=200,  # This is a simple test
+        max_gen=100,  # This is a simple test
         logger_file="ilogp.log",
         verbose=True,
     )
@@ -82,7 +82,7 @@ def test_selfies():
         fitness_function=fitness_function_selfies(
             3
         ),  # See fitness_functions_selfies, this is mw
-        max_gen=200,  # This is a simple test
+        max_gen=100,  # This is a simple test
         logger_file="mw.log",
         verbose=True,
     )
@@ -108,8 +108,8 @@ def test_selfies():
         fitness_function=fitness_function_selfies(
             6
         ),  # See fitness_functions_selfies, this is mv
-        max_gen=10,  # This is a simple test and this run is more expensive
-        pop_size=50,  # So we reduce the size of everything
+        max_gen=5,  # This is a simple test and this run is more expensive
+        pop_size=10,  # So we reduce the size of everything
         logger_file="mv.log",
         verbose=True,
     )
